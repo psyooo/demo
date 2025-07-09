10b44ca0de8e6d0581a96761a119f906bd115237
psyooo <167336643+psyooo@users.noreply.github.com>
2025-07-09 20:18:43 +0800

    network_s2.py

--- model/network_s2.py
+++ model/network_s2.py
@@ -1,28 +1,19 @@
-# -*- coding: utf-8 -*-
-"""
-❗❗❗❗❗❗李嘉鑫  作者微信 BatAug
-空天信息创新研究院20-25直博生，导师高连如
-
-"""
-"""
-❗❗❗❗❗❗#此py作用：第二阶段所需要的网络模块
-"""
 import torch
 from torch.nn import init
 import torch.nn as nn
 import numpy as np
 import os
 import scipy
-import torch.nn.functional as fun
+import torch.nn.functional as F
 from einops import rearrange
-
 
 
 def init_weights(net, init_type, gain):
     print('in init_weights')
+
     def init_func(m):
         classname = m.__class__.__name__
-        #print(classname,m,'_______')
+        # print(classname,m,'_______')
         if hasattr(m, 'weight') and (classname.find('Conv') != -1 or classname.find('Linear') != -1):
             if init_type == 'normal':
                 init.normal_(m.weight.data, 0.0, gain)
@@ -34,10 +25,10 @@ def init_weights(net, init_type, gain):
                 init.orthogonal_(m.weight.data, gain=gain)
             elif init_type == 'mean_space':
                 batchsize, channel, height, weight = list(m.weight.data.size())
-                m.weight.data.fill_(1/(height*weight))
+                m.weight.data.fill_(1 / (height * weight))
             elif init_type == 'mean_channel':
                 batchsize, channel, height, weight = list(m.weight.data.size())
-                m.weight.data.fill_(1/(channel))
+                m.weight.data.fill_(1 / (channel))
             else:
                 raise NotImplementedError('initialization method [%s] is not implemented' % init_type)
             if hasattr(m, 'bias') and m.bias is not None:
@@ -45,142 +36,92 @@ def init_weights(net, init_type, gain):
         elif classname.find('BatchNorm2d') != -1:
             init.normal_(m.weight.data, 1.0, gain)
             init.constant_(m.bias.data, 0.0)
-    
+
     print('initialize network with %s' % init_type)
     net.apply(init_func)
 
-def init_net(net,device, init_type, init_gain,initializer):
+
+def init_net(net, device, init_type, init_gain, initializer):
     print('in init_net')
     net.to(device)  # gpu_ids[0] 是 gpu_ids列表里面的第一个int值
-    if initializer :
+    if initializer:
         # print(2,initializer)
-        init_weights(net,init_type, init_gain)
+        init_weights(net, init_type, init_gain)
     else:
-        print('Spectral_downsample with default initialize')
+        print('Spatial_upsample with default initialize')
     return net
 
-    
 
 
-#########################  def_two_stream_interactive双流交叉光谱学习网络 #########################
+def create_inverse_PSF(sigma, shape=(3, 3), device='cpu'):
+    """
+    直接在网络文件里生成逆PSF核。
+    """
+    m, n = [(ss - 1.0) / 2.0 for ss in shape]
+    y, x = torch.meshgrid(torch.arange(-m, m + 1), torch.arange(-n, n + 1), indexing='ij')
+    h = torch.exp(-(x * x + y * y) / (2.0 * sigma * sigma))
+    h[h < torch.finfo(h.dtype).eps * h.max()] = 0
+    sumh = h.sum()
+    if sumh != 0:
+        h /= sumh
+    # 可以简单地认为高斯核就是近似的逆PSF（物理上更建议用Wiener逆，但常用这种简化）
+    return h.to(device)
 
-
-def def_two_stream_interactive(msi_channels,hsi_channels,device,init_type='kaiming', init_gain=0.02,initializer=True):
-
-    net = two_stream_interactive(msi_channels,hsi_channels)
-    
-    return init_net(net, device, init_type, init_gain ,initializer)
-
-
-class two_stream_interactive(nn.Module):
-    def __init__(self,msi_channels,hsi_channels,need_clamp=False):
+class InversePSF(nn.Module):
+    def __init__(self, inv_psf, scale):
+        """
+        inv_psf: torch.Tensor, shape (k, k)
+        scale: int, 卷积核大小和stride（等于目标上采样倍数）
+        """
         super().__init__()
-        self.layers=[]  # 空列表，用来存放网络层
-        self.need_clamp=need_clamp  # 是否需要对输出进行clamp操作
-        self.num_ups=int(np.log2(hsi_channels/msi_channels))    # 计算需要多少次上采样：log2（C/c）
-        self.lrhsi_stream=nn.ModuleList([])  # 低分光谱流
-        self.hrmsi_stream=nn.ModuleList([])  # 高分光谱流
+        k = inv_psf.shape[0]
+        self.convT = nn.ConvTranspose2d(
+            1, 1, kernel_size=k, stride=scale, padding=0, bias=False
+        )
+        self.convT.weight.data[0, 0] = inv_psf.float()
+        self.convT.weight.requires_grad = True
 
-        self.lrhsi_new_stream = nn.ModuleList([])
-        self.hrmsi_new_stream = nn.ModuleList([])
-        
-        for i in range(1,self.num_ups+1): # 1 2 3 4
-            feature_dim = msi_channels * (2 ** (i - 1))
-            self.lrhsi_stream.append(spe(feature_dim,msi_channels*(2**(i))))
-                                     
-            self.hrmsi_stream.append(spe(feature_dim,msi_channels*(2**(i))))
+    def forward(self, x):
+        # x: [B, 1, h, w]
+        return self.convT(x)
 
-
-
-                                     
-        self.lrhsi_stream.append(nn.Conv2d(msi_channels*(2**self.num_ups), hsi_channels, kernel_size=1, stride=1, padding=0))     # 最后一层卷积层
-        
-        self.hrmsi_stream.append(nn.Conv2d(msi_channels*(2**self.num_ups), hsi_channels, kernel_size=1, stride=1, padding=0))     # 最后一层卷积层
-
-
-    def forward(self,lrmsi_flrhsi,lrmsi_fhrmsi):
-        for i in range(1,self.num_ups+1):
-
-            lrmsi_flrhsi = self.lrhsi_stream[i-1](lrmsi_flrhsi)  # 低分光谱流K1
-            lrmsi_fhrmsi = self.hrmsi_stream[i-1](lrmsi_fhrmsi)  # 高分光谱流K2
-            #print(lrmsi_flrhsi.shape,lrmsi_fhrmsi.shape)
-
-            # 低分光谱流和高分光谱流交叉相加
-            lrmsi_flrhsi=lrmsi_flrhsi+lrmsi_fhrmsi  # 低分光谱流和高分光谱流相加
-            lrmsi_fhrmsi=lrmsi_fhrmsi+lrmsi_flrhsi  # 高分光谱流和低分光谱流相加
-            #print(lrmsi_flrhsi.shape,lrmsi_fhrmsi.shape)
-        
-        out_lrmsi_flrhsi=self.lrhsi_stream[-1](lrmsi_flrhsi)    # 低分光谱流输出
-        out_lrmsi_fhrmsi=self.hrmsi_stream[-1](lrmsi_fhrmsi)    # 高分光谱流输出
-
-        return out_lrmsi_flrhsi,out_lrmsi_fhrmsi
-#########################  def_two_stream_interactive #########################
-
-
-
-########################## SPE:交叉通道交互增强模块 ############################
-
-
-class spe(nn.Module):
-    def __init__(self,input_channel,output_channel):
+class MatrixDotLR2HR(nn.Module):
+    def __init__(self, inv_psf, scale, max_channels):
         super().__init__()
-    
-        self.begin=nn.Sequential(
-            nn.Conv2d(in_channels=input_channel,out_channels=60,kernel_size=(1,1),stride=1,padding=0) ,
-            nn.LeakyReLU(0.2, inplace=True)
-            )
-        
-        
-        self.stream1= nn.Sequential(
-            nn.Conv2d(in_channels=20,out_channels=20,kernel_size=(1,1),stride=1,padding=0),
-            nn.LeakyReLU(0.2, inplace=True)
-            )
+        self.n_channels = max_channels
+        self.band_layers = nn.ModuleList([
+            InversePSF(inv_psf, scale) for _ in range(max_channels)
+        ])
 
-        self.stream2=nn.Sequential(
-            nn.Conv2d(in_channels=20,out_channels=20,kernel_size=(1,1),stride=1,padding=0),
-            nn.LeakyReLU(0.2, inplace=True)
-            )
-        
-        self.stream3=nn.Sequential(
-            nn.Conv2d(in_channels=20,out_channels=20,kernel_size=(1,1),stride=1,padding=0),
-            nn.LeakyReLU(0.2, inplace=True)
-            )
+    def forward(self, x):
+        # x: [B, C, h, w]
+        batch, channel, height, width = x.size()
+        outs = [self.band_layers[i](x[:, i:i+1, :, :]) for i in range(channel)]  # [B, 1, H, W]
+        out = torch.cat(outs, dim=1)  # [B, C, H, W]
+        return out
 
-        self.end=nn.Sequential(
-            nn.Conv2d(in_channels=60,out_channels=output_channel,kernel_size=(1,1),stride=1,padding=0),
-            nn.LeakyReLU(0.2, inplace=True)
-            )
-        
+class SpaceUpNet(nn.Module):
+    """双流空间超分主干，与EDIP双流结构一致。支持两路输入，分支可以同构或异构"""
+    def __init__(self, in_ch, out_ch, scale=2):
+        super().__init__()
+        # 分支1
+        self.branch1 = nn.Sequential(
+            nn.Conv2d(in_ch, 64, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Upsample(scale_factor=scale, mode='bilinear', align_corners=False),
+            nn.Conv2d(64, out_ch, 3, padding=1)
+        )
+        # 分支2（可以同构，也可以设计成不同结构）
+        self.branch2 = nn.Sequential(
+            nn.Conv2d(in_ch, 64, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Upsample(scale_factor=scale, mode='bilinear', align_corners=False),
+            nn.Conv2d(64, out_ch, 3, padding=1)
+        )
+        # 可以插入交互/融合/残差等模块
 
-        
-    def forward(self,input):
-        
-        x1=self.begin(input) # torch.Size([1, 60, 50, 50])  input:torch.Size([1, 100, 50, 50])
-
-        # 拆分光谱波段
-        split1=x1[:,0:20,:,:]   # torch.Size([1, 20, 50, 50])
-        split2=x1[:,20:40,:,:]  # torch.Size([1, 20, 50, 50])
-        split3=x1[:,40:,:,:]    # torch.Size([1, 20, 50, 50])
-
-        # 中间层
-        middle1=self.stream1(split1)    # torch.Size([1, 20, 50, 50])：0-20波段
-        middle2=self.stream2(split2+middle1)    # torch.Size([1, 20, 50, 50])：20-40波段
-        middle3=self.stream3(split3+middle2)    # torch.Size([1, 20, 50, 50])：40-60波段
-        
-        concat=torch.cat([ middle1, middle2,middle3 ], dim=1)   # torch.Size([1, 60, 50, 50])拼接
-        
-        x2=x1+concat    # torch.Size([1, 60, 50, 50])残差连接
-        
-        out=self.end(x2)    # torch.Size([1, 100, 50, 50])
-        
-        return out 
-
-########################## SPE ############################
-
-
-if __name__ == "__main__":
-    import numpy as np
-    import os
-    import scipy.io as io
-
-    
+    def forward(self, x1, x2):
+        # x1, x2 分别为两路输入（如Lr-MSI1和Lr-MSI2）
+        y1 = self.branch1(x1)
+        y2 = self.branch2(x2)
+        return y1, y2
