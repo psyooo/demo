fa5b8165c1c746f7b9a70574ebdb2527c3175ba7
psyooo <167336643+psyooo@users.noreply.github.com>
2025-07-21 09:39:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 """
 ❗❗❗❗❗❗#主文件
 """
+import os
 
 import torch
 import time
@@ -38,6 +39,8 @@ psf = blind.model.psf.data.cpu().detach().numpy()[0,0,:,:] #15 x 15  numpy
 srf = blind.model.srf.data.cpu().detach().numpy()[:,:,0,0].T #46 x 8   numpy
 psf_gt=blind.psf_gt #15 x 15   numpy
 srf_gt=blind.srf_gt  #46 x 8   numpy
+gt=blind.tensor_gt #1 C H W张量
+
 
 end = time.perf_counter()   # 记录结束时间
 elapsed_S1 = end - start        # 计算经过的时间（单位为秒）
@@ -61,17 +64,24 @@ elapsed_S1 = end - start        # 计算经过的时间（单位为秒）
 # spectral_sr=spectral_SR(args,lr_msi_fhsi_est.clone().detach(), lr_msi_fmsi_est.clone().detach(),  #lr_msi_fhsi_est, lr_msi_fmsi_est
 #                            blind.tensor_lr_hsi,blind.tensor_hr_msi,blind.gt)
 # start = time.perf_counter() # 记录开始时间
-# Out_fused_s2 =spectral_sr.train() #返回的是四维tensor device上
+# hr_hsi1, hr_hsi2 =spectral_sr.train() #返回的是四维tensor device上
 # end = time.perf_counter()   # 记录结束时间
 # elapsed_S2 = end - start        # 计算经过的时间（单位为秒）
 
-from model.spatial_up import spatial_SR
-spatial_sr = spatial_SR(args, lr_msi_fhsi_est.clone().detach(), lr_msi_fmsi_est.clone().detach(),blind.tensor_lr_hsi,blind.tensor_hr_msi,blind.gt,blind.srf_gt) #, lr_hsi, hr_msi)
+# from model.spatial_up import spatial_SR
+# spatial_sr = spatial_SR(args, lr_msi_fhsi_est.clone().detach(), lr_msi_fmsi_est.clone().detach(),blind.tensor_lr_hsi,blind.tensor_hr_msi,blind.gt,blind.srf_gt) #, lr_hsi, hr_msi)
+# start = time.perf_counter() # 记录开始时间
+# hr_hsi1, hr_hsi2 = spatial_sr.train()
+# end = time.perf_counter()   # 记录结束时间
+# elapsed_S2 = end - start        # 计算经过的时间（单位为秒）
+
+from model.CP import CP_model
+CP=CP_model(args,blind.tensor_hr_msi,blind.tensor_lr_hsi,psf_gt,srf_gt,blind)
+print(CP.net)
 start = time.perf_counter() # 记录开始时间
-hr_hsi1, hr_hsi2 = spatial_sr.train()
+hr_hsi1, hr_hsi2 =CP.train() # 返回的是四维tensor device上
 end = time.perf_counter()   # 记录结束时间
 elapsed_S2 = end - start        # 计算经过的时间（单位为秒）
-
 
 
 
@@ -80,6 +90,7 @@ elapsed_S2 = end - start        # 计算经过的时间（单位为秒）
 
 from model.dip import dip #my_dip  ,  dip 
 # DIP=dip(args,Out_fused_s2 ,Out_fused_s2 ,psf,srf,blind)
+# DIP=dip(args,hr_hsi1 ,hr_hsi2 ,psf,srf,blind)
 DIP=dip(args,hr_hsi1 ,hr_hsi2 ,psf,srf,blind)
 # 【3】把物理先验传给dip
 # DIP = dip(
@@ -90,7 +101,7 @@ DIP=dip(args,hr_hsi1 ,hr_hsi2 ,psf,srf,blind)
 # )
 print(DIP.net)
 start = time.perf_counter() # 记录开始时间  
-out_fhsi_s3,out_fmsi_s3=DIP.train() # 返回的是四维tensor device上
+out_trans_s3,out_unet_s3=DIP.train() # 返回的是四维tensor device上
 end = time.perf_counter()   # 记录结束时间
 elapsed_S3 = end - start        # 计算经过的时间（单位为秒）
 
@@ -100,7 +111,7 @@ elapsed_S3 = end - start        # 计算经过的时间（单位为秒）
 '''第四阶段'''
 from model.select import select_decision
 start = time.perf_counter() # 记录开始时间  
-srf_out=select_decision(out_fhsi_s3,out_fmsi_s3,blind) #返回的是3维H W C numpy 这个就是最终的结果
+srf_out=select_decision(out_trans_s3,out_unet_s3,blind) #返回的是3维H W C numpy 这个就是最终的结果
 
 end = time.perf_counter()   # 记录结束时间
 elapsed_S4 = end - start        # 计算经过的时间（单位为秒）
@@ -108,7 +119,7 @@ elapsed_S4 = end - start        # 计算经过的时间（单位为秒）
 
 # ====================可视化===========================
 hsi_bands = [45, 29, 13]  # 注意：Python索引从0开始，所以实际索引为46-1, 30-1, 14-1
-msi_bands = [7, 5, 3]
+msi_bands = [5, 4, 2]
 
 # 第一阶段结果可视化
 lr_msi_fhsi_est_numpy = lr_msi_fhsi_est.data.cpu().detach().numpy()[0].transpose(1, 2, 0)
@@ -119,46 +130,66 @@ hr_hsi1_numpy = hr_hsi1.data.cpu().detach().numpy()[0].transpose(1, 2, 0)
 hr_hsi2_numpy = hr_hsi2.data.cpu().detach().numpy()[0].transpose(1, 2, 0)
 
 # 第三阶段结果可视化
-out_fhsi_s3_numpy = out_fhsi_s3.data.cpu().detach().numpy()[0].transpose(1, 2, 0)
-out_fmsi_s3_numpy = out_fmsi_s3.data.cpu().detach().numpy()[0].transpose(1, 2, 0)
+out_trans_s3_numpy = out_trans_s3.data.cpu().detach().numpy()[0].transpose(1, 2, 0)
+out_unet_s3_numpy = out_unet_s3.data.cpu().detach().numpy()[0].transpose(1, 2, 0)
+
+# 将GT转为numpy
+gt_numpy = gt.data.cpu().detach().numpy()[0].transpose(1, 2, 0)
+# 确保 srf_out 也是一个 PyTorch 张量，并进行相同的处理
+
 
 # 可视化每个阶段的结果
 plt.figure(figsize=(15, 10))
 
 # 第一阶段结果可视化
-plt.subplot(3, 2, 1)
+plt.subplot(2, 4, 1)
 plt.imshow(lr_msi_fhsi_est_numpy[:, :, msi_bands])
-plt.title('Stage 1: lr_msi(lr_hsi光谱退化)')
+plt.title('Stage 1: lr_msi(lr_hsi)')
 
-plt.subplot(3, 2, 2)
+plt.subplot(2, 4, 2)
 plt.imshow(lr_msi_fmsi_est_numpy[:, :, msi_bands])
-plt.title('Stage 1: lr_msi(hr_msi空间退化)')
+plt.title('Stage 1: lr_msi(hr_msi)')
 
 # 第二阶段结果可视化
 hr_hsi1_rgb = hr_hsi1_numpy[:, :, hsi_bands]
 hr_hsi2_rgb = hr_hsi2_numpy[:, :, hsi_bands]
 
-plt.subplot(3, 2, 3)
+plt.subplot(2, 4, 3)
 plt.imshow(hr_hsi1_rgb)
 plt.title('Stage 2: hr_hsi1')
 
-plt.subplot(3, 2, 4)
+plt.subplot(2, 4, 4)
 plt.imshow(hr_hsi2_rgb)
 plt.title('Stage 2: hr_hsi2')
 
 # 第三阶段结果可视化
-out_fhsi_s3_rgb = out_fhsi_s3_numpy[:, :, hsi_bands]
-out_fmsi_s3_rgb = out_fmsi_s3_numpy[:, :, hsi_bands]
+out_trans_s3_rgb = out_trans_s3_numpy[:, :, hsi_bands]
+out_unet_s3_rgb = out_unet_s3_numpy[:, :, hsi_bands]
 
-plt.subplot(3, 2, 5)
-plt.imshow(out_fhsi_s3_rgb)
-plt.title('Stage 3: out_fhsi_s3')
+plt.subplot(2, 4, 5)
+plt.imshow(out_trans_s3_rgb)
+plt.title('Stage 3: out_trans_s3')
 
-plt.subplot(3, 2, 6)
-plt.imshow(out_fmsi_s3_rgb)
-plt.title('Stage 3: out_fmsi_s3')
+plt.subplot(2, 4, 6)
+plt.imshow(out_unet_s3_rgb)
+plt.title('Stage 3: out_unet_s3')
+
+
+
+# 第四阶段结果可视化
+gt_rgb = gt_numpy[:, :, hsi_bands]
+srf_out_rgb = srf_out[:, :, hsi_bands]
+
+plt.subplot(2, 4, 8)
+plt.imshow(gt_rgb)
+plt.title('gt_hsi')
+
+plt.subplot(2, 4, 7)
+plt.imshow(srf_out_rgb)
+plt.title('Stage 4: final result')
 
 plt.tight_layout()
+plt.savefig(os.path.join(blind.args.expr_dir, 'visualization_results.png')) # 保存图片
 plt.show()
 
 # 计算最终融合图像与gt图的RMSE热图、SAM热图和重建误差图
@@ -176,30 +207,36 @@ def compute_sam_map(gt, pred):
     cos_theta = dot_product / (norm_gt * norm_pred)
     sam_map = np.arccos(cos_theta).reshape(gt.shape[:2])
     return sam_map
+# 将srf_out转为tensor
 
-rmse_map = compute_rmse_map(gt, srf_out)
-sam_map = compute_sam_map(gt, srf_out)
-reconstruction_error_map = np.abs(gt - srf_out)
+srf_out_tensor = torch.from_numpy(srf_out).unsqueeze(0).to(gt.device)
+srf_out_tensor = srf_out_tensor.permute(0, 3, 1, 2)  # 维度变为 (1, 46, 240, 240)
+rmse_map = compute_rmse_map(gt_numpy, srf_out)
+sam_map = compute_sam_map(gt_numpy, srf_out)
+reconstruction_error_map = np.abs(gt_numpy - srf_out)
 
 # 可视化RMSE热图、SAM热图和重建误差图
 plt.figure(figsize=(15, 5))
 
 plt.subplot(1, 3, 1)
-plt.imshow(rmse_map, cmap='hot')
+plt.imshow(rmse_map, cmap='jet')
 plt.title('RMSE Map')
 plt.colorbar()
 
 plt.subplot(1, 3, 2)
-plt.imshow(sam_map, cmap='hot')
+plt.imshow(sam_map, cmap='jet')
 plt.title('SAM Map')
 plt.colorbar()
 
 plt.subplot(1, 3, 3)
-plt.imshow(reconstruction_error_map[:, :, 0], cmap='hot')
+plt.imshow(reconstruction_error_map[:, :, 0], cmap='jet')
 plt.title('Reconstruction Error Map')
+
 plt.colorbar()
 
 plt.tight_layout()
+plt.savefig('Error_maps.png')  # 保存图片
+plt.savefig(os.path.join(blind.args.expr_dir, 'Error_maps.png'))
 plt.show()
 
 def get_parameter_number(net):
@@ -208,16 +245,8 @@ def get_parameter_number(net):
     return {'Total': total_num, 'Trainable': trainable_num}
 
 print(get_parameter_number(blind.model))
-print(get_parameter_number(spatial_sr.space_sr_net_msi))
+print(get_parameter_number(spectral_sr.two_stream))
 print(get_parameter_number(DIP.net))
 
 print("training time S1:{},s2:{},s3:{},s4:{}".format(elapsed_S1,elapsed_S2,elapsed_S3,elapsed_S4))
-
-
-
-
-
-
-
-
 
